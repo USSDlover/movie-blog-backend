e4d4553c9e33b1b7d394866ad45133a92721935d
Alireza Razinejad <a.razinejad@gmail.com>
2021-09-23 14:00:05 +0330

    fix: Get one movie now extract the ID

--- cmd/api/movie-handler.go
+++ cmd/api/movie-handler.go
@@ -1,9 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+)
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
 
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Print(errors.New("invalid id parameter"))
+	}
+
+	app.logger.Println("id is", id)
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
