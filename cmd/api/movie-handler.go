c54c72a08d59b3ff0aacdc8b5799a27dcd0c668a
Alireza Razinejad <a.razinejad@gmail.com>
2021-09-23 12:24:10 +0330

    feat: Initialized movie handler

---
+++ cmd/api/movie-handler.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
